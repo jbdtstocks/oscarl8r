5ab212937fdca9e3a37f8085ebb83ef6911ec681
jbdtstocks <jbdtstocks@gmail.com>
2018-07-14 12:16:06 -0500

    delete extras

--- Candles.py
+++ Candles.py
@@ -48,14 +48,12 @@ def FullAnalysis(span=4):
 		df = pdr.DataReader(ticker, 'iex', start='1/1/2018')
 		closing_prices = df['close'].tolist()
 		dates = df.index.tolist()
-		# (df['close']).plot()
 		df['L14'] = df['low'].rolling(window=14).min()
 		df['H14'] = df['high'].rolling(window=14).max()
 		df['%K'] = 100*((df['close'].iloc[-1] - df['L14']) / (df['H14'] - df['L14']))
 		df['%D'] = df['%K'].rolling(window=3).mean()
 		D = df['%D'].tolist()
 		K = df['%K'].tolist()
-		# (df[['%K', '%D']]).plot()
 		close = df['close']
 		delta = close.diff()
 		delta = delta[1:]
@@ -66,11 +64,6 @@ def FullAnalysis(span=4):
 		roll_down = (down.abs()).ewm(span).mean()
 		RS1 = roll_up / roll_down
 		df['RSI'] = 100.0 - (100.0 / (1.0 + RS1))
-		seventy_perc = 70
-		thirty_perc = 30
-		# RSI.plot()
-		# plt.axhline(y=70, color='r')
-		# plt.axhline(y=30, color='g')
 		df['70%'] = 70
 		df['30%'] = 30
 		df[['%K', '%D', 'close', 'RSI', '30%', '70%']].plot()
