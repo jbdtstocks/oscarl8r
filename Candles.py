5019f65c5d58bd2d591a62569659ea23d303ee66
jbdtstocks <jbdtstocks@gmail.com>
2018-07-14 14:50:07 -0500

    fix

--- Candles.py
+++ Candles.py
@@ -42,52 +42,52 @@ def StockStatistics(ticker):
 	pass
 
 def Forecast(ticker):
-	df = pdr.DataReader(ticker, 'iex', start='1/1/2018')
+	df = pdr.DataReader(ticker, 'quandl', start='1/1/2006', end='1/1/2016')
 	new = pd.DataFrame()
-	new = new.append(df['close'])
+	new = new.append(df['AdjClose'])
 	new = new.T
 	new['ds'] = new.index
-	new['y'] = new['close']
+	new['y'] = new['AdjClose']
 	cols = new.columns.tolist()
-	cols.remove('close')
+	cols.remove('AdjClose')
 	new = new[cols]
 	m = Prophet()
 	m.fit(new)
-	future = m.make_future_dataframe(periods=180)
+	future = m.make_future_dataframe(periods=730)
 	forecast = m.predict(future)
 	m.plot(forecast)
-	m.plot_components(forecast)
 	plt.show()
+	# m.plot_components(forecast)
 
-def FullAnalysis(span=4):
+def FullAnalysis(ticker, span=4):
 	path = "C:/Users/david/oscarl8r/workingtickers.xlsx"
 	tickers = pd.read_excel(path)
 	tickers = tickers['TICKER'].tolist()
-	for ticker in tickers:
-		df = pdr.DataReader(ticker, 'iex', start='1/1/2018')
-		closing_prices = df['close'].tolist()
-		dates = df.index.tolist()
-		df['L14'] = df['low'].rolling(window=14).min()
-		df['H14'] = df['high'].rolling(window=14).max()
-		df['%K'] = 100*((df['close'].iloc[-1] - df['L14']) / (df['H14'] - df['L14']))
-		df['%D'] = df['%K'].rolling(window=3).mean()
-		D = df['%D'].tolist()
-		K = df['%K'].tolist()
-		close = df['close']
-		delta = close.diff()
-		delta = delta[1:]
-		up, down = delta.copy(), delta.copy()
-		up[up < 0] = 0
-		down[down > 0] = 0
-		roll_up = up.ewm(span).mean()
-		roll_down = (down.abs()).ewm(span).mean()
-		RS1 = roll_up / roll_down
-		df['RSI'] = 100.0 - (100.0 / (1.0 + RS1))
-		df['70%'] = 70
-		df['30%'] = 30
-		df[['%K', '%D', 'close', 'RSI', '30%', '70%']].plot()
-		plt.title(ticker)
-		plt.show()
+	# for ticker in tickers:
+	df = pdr.DataReader(ticker, 'iex', start='1/1/2018')
+	closing_prices = df['close'].tolist()
+	dates = df.index.tolist()
+	df['L14'] = df['low'].rolling(window=14).min()
+	df['H14'] = df['high'].rolling(window=14).max()
+	df['%K'] = 100*((df['close'].iloc[-1] - df['L14']) / (df['H14'] - df['L14']))
+	df['%D'] = df['%K'].rolling(window=3).mean()
+	D = df['%D'].tolist()
+	K = df['%K'].tolist()
+	close = df['close']
+	delta = close.diff()
+	delta = delta[1:]
+	up, down = delta.copy(), delta.copy()
+	up[up < 0] = 0
+	down[down > 0] = 0
+	roll_up = up.ewm(span).mean()
+	roll_down = (down.abs()).ewm(span).mean()
+	RS1 = roll_up / roll_down
+	df['RSI'] = 100.0 - (100.0 / (1.0 + RS1))
+	df['70%'] = 70
+	df['30%'] = 30
+	df[['%K', '%D', 'close', 'RSI', '30%', '70%']].plot()
+	plt.title(ticker)
+	plt.show()
 
-# FullAnalysis()
-Forecast('AAPL')
+FullAnalysis('AAPL')
+# Forecast('AAPL')
