7e212a845c05e9a9bdcec3fcfe81da42713144b2
jbdtstocks <jbdtstocks@gmail.com>
2018-07-15 17:05:25 -0500

    variable generalizations

--- Candles.py
+++ Candles.py
@@ -1,7 +1,5 @@
 from imports import *
 
-ticker = 'AAPL'
-
 def MarketData(ticker):
 	df = pdr.DataReader(ticker, 'iex', start='1/1/2018')
 	closing_prices = df['close'].tolist()
@@ -41,15 +39,27 @@ def RelativeStrengthIndex(ticker):
 def StockStatistics(ticker):
 	pass
 
-def Forecast(ticker):
-	df = pdr.DataReader(ticker, 'quandl', start='1/1/2006', end='1/1/2016')
+def Forecast(ticker, api='quandl', start='1/1/2006', end='4/1/2018'):
+	df = pdr.DataReader(ticker, api, start, end)
 	new = pd.DataFrame()
-	new = new.append(df['AdjClose'])
+	if api == 'quandl':
+		open = 'AdjOpen'
+		close = 'AdjClose'
+		high = 'AdjHigh'
+		low = 'AdjLow'
+		volume = 'AdjVolume'
+	if api == 'iex':
+		open = 'open'
+		close = 'close'
+		high = 'high'
+		low = 'low'
+		volume = 'volume'
+	new = new.append(df[close])
 	new = new.T
 	new['ds'] = new.index
-	new['y'] = new['AdjClose']
+	new['y'] = new[close]
 	cols = new.columns.tolist()
-	cols.remove('AdjClose')
+	cols.remove(close)
 	new = new[cols]
 	m = Prophet(changepoint_prior_scale=0.15)
 	m.fit(new)
@@ -60,17 +70,29 @@ def Forecast(ticker):
 	plt.show()
 	# m.plot_components(forecast)
 
-def FullAnalysis(ticker, span=4):
+def FullAnalysis(ticker, api='iex', span=4):
 	path = "C:/Users/david/oscarl8r/workingtickers.xlsx"
 	tickers = pd.read_excel(path)
 	tickers = tickers['TICKER'].tolist()
+	if api == 'quandl':
+		open = 'AdjOpen'
+		close = 'AdjClose'
+		high = 'AdjHigh'
+		low = 'AdjLow'
+		volume = 'AdjVolume'
+	if api == 'iex':
+		open = 'open'
+		close = 'close'
+		high = 'high'
+		low = 'low'
+		volume = 'volume'
 	for ticker in tickers:
-		df = pdr.DataReader(ticker, 'iex', start='1/1/2018')
-		closing_prices = df['close'].tolist()
+		df = pdr.DataReader(ticker, api, start='1/1/2018')
+		closing_prices = df[close].tolist()
 		dates = df.index.tolist()
-		df['L14'] = df['low'].rolling(window=14).min()
-		df['H14'] = df['high'].rolling(window=14).max()
-		df['%K'] = 100*((df['close'].iloc[-1] - df['L14']) / (df['H14'] - df['L14']))
+		df['L14'] = df[low].rolling(window=14).min()
+		df['H14'] = df[high].rolling(window=14).max()
+		df['%K'] = 100*((df[close].iloc[-1] - df['L14']) / (df['H14'] - df['L14']))
 		df['%D'] = df['%K'].rolling(window=3).mean()
 		D = df['%D'].tolist()
 		K = df['%K'].tolist()
@@ -86,7 +108,7 @@ def FullAnalysis(ticker, span=4):
 		df['RSI'] = 100.0 - (100.0 / (1.0 + RS1))
 		df['70%'] = 70
 		df['30%'] = 30
-		df[['%K', '%D', 'close', 'RSI', '30%', '70%']].plot()
+		df[['%K', '%D', close, 'RSI', '30%', '70%']].plot()
 		# df['close'].plot()
 		plt.title(ticker)
 		plt.show()
