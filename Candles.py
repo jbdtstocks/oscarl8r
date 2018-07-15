29149ba7e82b320644fd4feb350c89414e471f5c
jbdtstocks <jbdtstocks@gmail.com>
2018-07-15 16:46:04 -0500

    edit forecast machine learning model

--- Candles.py
+++ Candles.py
@@ -51,11 +51,12 @@ def Forecast(ticker):
 	cols = new.columns.tolist()
 	cols.remove('AdjClose')
 	new = new[cols]
-	m = Prophet()
+	m = Prophet(changepoint_prior_scale=0.15)
 	m.fit(new)
-	future = m.make_future_dataframe(periods=730)
+	future = m.make_future_dataframe(periods=1095)
 	forecast = m.predict(future)
 	m.plot(forecast)
+	plt.title(ticker)
 	plt.show()
 	# m.plot_components(forecast)
 
@@ -63,31 +64,32 @@ def FullAnalysis(ticker, span=4):
 	path = "C:/Users/david/oscarl8r/workingtickers.xlsx"
 	tickers = pd.read_excel(path)
 	tickers = tickers['TICKER'].tolist()
-	# for ticker in tickers:
-	df = pdr.DataReader(ticker, 'iex', start='1/1/2018')
-	closing_prices = df['close'].tolist()
-	dates = df.index.tolist()
-	df['L14'] = df['low'].rolling(window=14).min()
-	df['H14'] = df['high'].rolling(window=14).max()
-	df['%K'] = 100*((df['close'].iloc[-1] - df['L14']) / (df['H14'] - df['L14']))
-	df['%D'] = df['%K'].rolling(window=3).mean()
-	D = df['%D'].tolist()
-	K = df['%K'].tolist()
-	close = df['close']
-	delta = close.diff()
-	delta = delta[1:]
-	up, down = delta.copy(), delta.copy()
-	up[up < 0] = 0
-	down[down > 0] = 0
-	roll_up = up.ewm(span).mean()
-	roll_down = (down.abs()).ewm(span).mean()
-	RS1 = roll_up / roll_down
-	df['RSI'] = 100.0 - (100.0 / (1.0 + RS1))
-	df['70%'] = 70
-	df['30%'] = 30
-	df[['%K', '%D', 'close', 'RSI', '30%', '70%']].plot()
-	plt.title(ticker)
-	plt.show()
+	for ticker in tickers:
+		df = pdr.DataReader(ticker, 'iex', start='1/1/2018')
+		closing_prices = df['close'].tolist()
+		dates = df.index.tolist()
+		df['L14'] = df['low'].rolling(window=14).min()
+		df['H14'] = df['high'].rolling(window=14).max()
+		df['%K'] = 100*((df['close'].iloc[-1] - df['L14']) / (df['H14'] - df['L14']))
+		df['%D'] = df['%K'].rolling(window=3).mean()
+		D = df['%D'].tolist()
+		K = df['%K'].tolist()
+		close = df['close']
+		delta = close.diff()
+		delta = delta[1:]
+		up, down = delta.copy(), delta.copy()
+		up[up < 0] = 0
+		down[down > 0] = 0
+		roll_up = up.ewm(span).mean()
+		roll_down = (down.abs()).ewm(span).mean()
+		RS1 = roll_up / roll_down
+		df['RSI'] = 100.0 - (100.0 / (1.0 + RS1))
+		df['70%'] = 70
+		df['30%'] = 30
+		df[['%K', '%D', 'close', 'RSI', '30%', '70%']].plot()
+		# df['close'].plot()
+		plt.title(ticker)
+		plt.show()
 
-FullAnalysis('AAPL')
-# Forecast('AAPL')
+# FullAnalysis('AAPL')
+Forecast('AAPL')
